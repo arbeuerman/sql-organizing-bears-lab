06c4485a4b3e0ea1deb0b2b6d63fbf39b02d54f6
Alexandra <arbeuerman@gmail.com>
2021-03-23 16:17:21 -0500

    practice with sql queries and table creation

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "dark brown", "kind", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 1, "M", "black and tan", "mischievous", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 4, "F", "light brown", "easy-going", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 6, "M", "dark brown", "aggressive", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 8, "F", "black", "protective", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 19, "M", "light brown", "grumpy", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 12, "F", "black", "playful", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 9, "M", "black and tan", "angry", false);
